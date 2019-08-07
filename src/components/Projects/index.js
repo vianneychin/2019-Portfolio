6dd1ad875c69561531798da296290e7053fcace2
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-08-07 12:25:18 -0700

    Import photos to component'

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { Wrapper } from './styled'
 import ProjectCard from './ProjectCard'
 
+import connectFourOne from '../../assets/screenshots/connect-four-one.png'
+import connectFourTwo from '../../assets/screenshots/connect-four-two.png'
+import californature from '../../assets/screenshots/californature.png'
+import weatherful from '../../assets/screenshots/weatherful.png'
+import muscleMaker from '../../assets/screenshots/muscle-maker.png'
+import safeParkingLA from '../../assets/screenshots/safe-parking-la.png'
+
 const Projects = () => {
   return (
     <Wrapper>
